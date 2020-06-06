e39f14b164c7f80636b0cf21f6a6e24d0f6301a1
Your name <you@your-domain.com>
2020-06-06 18:04:36 +0900

    Third

--- main.tf
+++ main.tf
@@ -11,16 +11,25 @@ resource "aws_instance" "example"{
   user_data = <<-EOF
               #!/bin/bash
               echo "hello,world" > index.html
-              nohup busybox httpd -f -p 8080 &
+              nohup busybox httpd -f -p "${var.server_port}" &
               EOF
 }
 
 resource "aws_security_group" "instance"{
   name = "terraform-example-instance"
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = "${var.server_port}"
+    to_port     = "${var.server_port}"
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+variable "server_port" {
+  description = "the port the server will use for HTTP requests"
+  default     = 8080
+}
+
+output "public_ip" {
+  value   = "${aws_instance.example.public_ip}"
 }
